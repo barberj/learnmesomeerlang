8d58beae6a0ab79d1695ca3e9479df1d3de74c60
Justin Barber <barber.justin@gmail.com>
2015-12-30 21:32:12 -0500

    update to use datetime rather then seconds for timeout

--- reminder/src/event.erl
+++ reminder/src/event.erl
@@ -32,11 +32,20 @@ cancel(Pid) ->
   end.
 
 %%% Event's innards
-init(Server, EventName, Delay) ->
-  loop(#state{server=Server, name=EventName, to_go=normalize(Delay)}).
+init(Server, EventName, DateTime) ->
+  loop(#state{server=Server, name=EventName, to_go=time_to_go(DateTime)}).
 
 %% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
 %% milliseconds, the following function is used
 normalize(N) ->
   Limit = 49*24*60*60,
   [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+time_to_go(TimeOut={{_,_,_}, {_,_,_}}) ->
+  Now = calendar:local_time(),
+  ToGo = calendar:datetime_to_gregorian_seconds(TimeOut) - calendar:datetime_to_gregorian_seconds(Now),
+  Secs = if
+    ToGo > 0 -> ToGo;
+    ToGo =< 0 -> 0
+  end,
+  normalize(Secs).
