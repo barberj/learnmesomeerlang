292f4b146151d728d39d4feed2bdabcd1989872b
Justin Barber <barber.justin@gmail.com>
2015-12-30 21:43:27 -0500

    add records

--- reminder/src/evserv.erl
+++ reminder/src/evserv.erl
@@ -1,6 +1,12 @@
 -module(evserv).
 -compile(export_all).
 
+%% state#events list of event records
+%% state#clients list of Client Pids
+-record(state, {events, clients}).
+
+-record(event, {name="", description="", pid, timeout={{1970,1,1},{0,0,0}}}).
+
 loop(State) ->
   receive
     {Pid, MsgRef, {subscribe, Client}} ->
